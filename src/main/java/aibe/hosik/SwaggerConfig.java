bbd3297c454e47ccccc19d2e9b0b7f0d87dda136
s0___0k <61587396+s0ooo0k@users.noreply.github.com>
2025-05-09 15:45:21 +0900

    fix : CORS 관련 이슈 해결

--- src/main/java/aibe/hosik/SwaggerConfig.java
+++ src/main/java/aibe/hosik/SwaggerConfig.java
@@ -2,7 +2,12 @@ package aibe.hosik;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;  // 이 import 추가
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -12,4 +17,12 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://team0.kro.kr")
+                ));
+    }
 }
