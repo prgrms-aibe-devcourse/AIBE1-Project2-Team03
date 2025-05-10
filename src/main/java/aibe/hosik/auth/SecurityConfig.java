1c74d53edfad710c67fd63da9d2c7334a767b5b7
cnncnncom <cnncnncom84@gmail.com>
2025-05-10 19:59:35 +0900

    feat : 구글 로그인 추가

--- src/main/java/aibe/hosik/auth/SecurityConfig.java
+++ src/main/java/aibe/hosik/auth/SecurityConfig.java
@@ -66,12 +66,6 @@ public class SecurityConfig {
                         .requestMatchers("/api/data/**").authenticated()
                         .anyRequest().permitAll()
                 )
-                /*.formLogin(form -> form
-                        .loginProcessingUrl("/auth/login")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .successHandler(authSuccessHandler())
-                )*/
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/auth/login")
                         .userInfoEndpoint(userlnfo -> userlnfo
@@ -138,6 +132,8 @@ public class SecurityConfig {
                     username = "github_" + user.getAttribute("login");
             case KAKAO  ->
                     username = "kakao_"  + user.getAttribute("id").toString();
+            case GOOGLE ->
+                    username = "google_" + user.getAttribute("sub").toString();
             default     -> throw new IllegalArgumentException("처리되지 않은 소셜 타입: " + socialType);
             }
 
