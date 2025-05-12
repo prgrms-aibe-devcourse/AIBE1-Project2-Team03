08069bea91683b57ed44e875e9bfb85081d6843c
klaus9267 <klaus9267@gmail.com>
2025-05-12 17:57:52 +0900

    feat: 리뷰 조회 구현

---
+++ src/main/java/aibe/hosik/review/dto/ReviewResponse.java
@@ -0,0 +1,21 @@
+package aibe.hosik.review.dto;
+
+import aibe.hosik.post.dto.PostDetailDTO;
+import aibe.hosik.post.entity.Post;
+import aibe.hosik.profile.dto.ProfileResponse;
+import aibe.hosik.review.Review;
+
+public record ReviewResponse(
+    Long id,
+    String content,
+    ProfileResponse reviewer,
+    Long postId,
+    String postTitle
+) {
+
+  public static ReviewResponse from(Review review) {
+    ProfileResponse profile = ProfileResponse.from(review.getReviewer().getProfile());
+    Post post = review.getPost();
+    return new ReviewResponse(review.getId(), review.getContent(), profile, post.getId(), post.getTitle());
+  }
+}
