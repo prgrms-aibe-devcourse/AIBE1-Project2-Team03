834cce9e68f18ae5ea72035de55cde7f32487b4a
klaus9267 <klaus9267@gmail.com>
2025-05-12 15:49:01 +0900

    feat: 소셜로그인 조회 비관적락 적용

--- src/main/java/aibe/hosik/user/UserRepository.java
+++ src/main/java/aibe/hosik/user/UserRepository.java
@@ -1,6 +1,8 @@
 package aibe.hosik.user;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // email로 사용자 찾기
     Optional<User> findByEmail(String email);
     // 소셜 타입과 소셜 ID로 사용자 찾기 (소셜 로그인 시 주로 사용)
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
-
 }
