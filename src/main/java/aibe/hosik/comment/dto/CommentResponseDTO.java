08436b53ce005d11c11eca5354abe8dddcdf0b70
klaus9267 <klaus9267@gmail.com>
2025-05-14 10:32:05 +0900

    fix: 댓글 목록 조회 응닶값 변환, varchar -> text로 변경

--- src/main/java/aibe/hosik/comment/dto/CommentResponseDTO.java
+++ src/main/java/aibe/hosik/comment/dto/CommentResponseDTO.java
@@ -3,6 +3,8 @@ package aibe.hosik.comment.dto;
 import aibe.hosik.comment.entity.Comment;
 import aibe.hosik.post.dto.PostResponseDTO;
 import aibe.hosik.post.entity.Post;
+import aibe.hosik.profile.Profile;
+import aibe.hosik.profile.dto.ProfileResponse;
 import aibe.hosik.user.User;
 
 import java.time.LocalDateTime;
@@ -12,22 +14,19 @@ import java.util.List;
 public record CommentResponseDTO(
         Long id,
         String content,
-        String username,
-        Long userId,
-        String profileImage,
         LocalDateTime createdAt,
+        ProfileResponse profile,
         // 부모 댓글 > 자식 댓글
         List<CommentResponseDTO> replies
 ) {
     public static CommentResponseDTO from(Comment comment) {
         User user = comment.getUser();
+        ProfileResponse profile = ProfileResponse.from(comment.getUser().getProfile());
         return new CommentResponseDTO(
                 comment.getId(),
                 comment.getContent(),
-                comment.getUser().getName(),
-                user.getId(),
-                user.getProfile().getImage(),
                 comment.getCreatedAt(),
+                profile,
                 new ArrayList<>()
         );
     }
